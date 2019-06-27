b435b968477d5d2b214b663153873558d9094915
Willie <willie_chang@emphasis.com.tw>
2019-06-27 14:50:48 +0800

    change Jewels and Stones submission use char instead of String split to improve performance

--- java/src/main/java/com/example/leetcode/JewelsAndStones.java
+++ java/src/main/java/com/example/leetcode/JewelsAndStones.java
@@ -3,12 +3,12 @@ package com.example.leetcode;
 public class JewelsAndStones {
 
     public int numJewelsInStones(String J, String S) {
-        String[] sSplit = S.split("");
-
+        char[] sSplit = S.toCharArray();
         int sum = 0;
-        for (String j : J.split("")) {
-            for (String s : sSplit) {
-                if (j.equals(s)) {
+
+        for (char j : J.toCharArray()) {
+            for (char s : sSplit) {
+                if (j == s) {
                     sum++;
                 }
             }

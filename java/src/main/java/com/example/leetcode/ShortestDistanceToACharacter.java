baca51f76069b8878db4a3d968f76699f7a2d1c4
Willie <willie_chang@emphasis.com.tw>
2019-07-05 15:40:04 +0800

    change shortest distance to a character submission use a simpler method

--- java/src/main/java/com/example/leetcode/ShortestDistanceToACharacter.java
+++ java/src/main/java/com/example/leetcode/ShortestDistanceToACharacter.java
@@ -3,41 +3,26 @@ package com.example.leetcode;
 public class ShortestDistanceToACharacter {
 
     int[] shortestToChar(String S, char C) {
-        char[] chars = S.toCharArray();
-        int[] result = new int[chars.length];
+        int length = S.length();
+        int[] result = new int[length];
+        int prev = -10000;
 
-        int preHold = indexOf(chars, C, -1);
-        int postHold = indexOf(chars, C, preHold);
-
-        for (int i = 0; i < chars.length; i++) {
-            if (postHold == -1 && i < preHold) {
-                result[i] = preHold - i;
-            } else if (postHold == -1 && i > preHold) {
-                result[i] = i - preHold;
-            } else if (i < preHold) {
-                result[i] = preHold - i;
-            } else if (i == preHold) {
-                result[i] = 0;
-            } else if (i > preHold && i < postHold) {
-                int preDis = i - preHold;
-                int postDis = postHold - i;
-                result[i] = preDis > postDis ? postDis : preDis;
-            } else if (i > preHold && i == postHold) {
-                result[i] = 0;
-                preHold = postHold;
-                postHold = indexOf(chars, C, postHold);
+        for (int i = 0; i < length; ++i) {
+            if (S.charAt(i) == C) {
+                prev = i;
             }
+            result[i] = i - prev;
+        }
+
+        prev = 10000;
+
+        for (int i = length - 1; i >= 0; --i) {
+            if (S.charAt(i) == C) {
+                prev = i;
+            }
+            result[i] = Math.min(result[i], prev - i);
         }
 
         return result;
-    }
-
-    int indexOf(char[] chars, char c, int fromIndex) {
-        for (int i = fromIndex + 1; i < chars.length; i++) {
-            if (chars[i] == c) {
-                return i;
-            }
-        }
-        return -1;
     }
 }
